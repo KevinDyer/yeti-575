a1c778a4cd83be3a871456efe16f6c294bc5ac3f
KevinDyer <kevin.m.dyer@gmail.com>
2015-05-24 10:02:48 -0600

    Started adding endpoint for file uploads.

--- app.js
+++ app.js
@@ -4,9 +4,15 @@
   var express = require('express');
   var app = express();
 
+  // Serve the dependencies
   app.use('/bower_components', express.static('bower_components'));
 
+  // Serve the app directory
   app.use(express.static('app'));
+
+  app.post('/uploads', function(request, response) {
+    response.sendStatus(201);
+  });
 
   app.listen(8000, function() {
     console.log('Listening on *8000');
--- app/javascript/controllers/upload-index-controller.js
+++ app/javascript/controllers/upload-index-controller.js
@@ -5,5 +5,12 @@
   .controller('UploadIndexController', ['$http', '$scope', function($http, $scope) {
     var self = this;
     $scope.title = 'Uploads';
+
+    $scope.uploadFile = function() {
+      $http({method: 'POST', url: '/uploads', data: {hello: 'world'}})
+      .success(function(data, status) {
+        console.log(status);
+      });
+    };
   }]);
 })(angular);
