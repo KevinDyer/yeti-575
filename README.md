bd3ad59209d55fbd41d497999f0d8240b0f78391
kevindy <kevindy@abasin.ninja>
2015-05-30 19:33:29 -0600

    Clear everything out.

--- README.md
+++
@@ -1,5 +0,0 @@
-# Yeti 575
-Visualization tool for project yeti
-
-# Installing
-npm install && bower install && npm start
--- app/javascript/app.js
+++
@@ -1,5 +0,0 @@
-(function (angular) {
-  'use strict';
-
-  angular.module('Yeti575', ['ngRoute', 'ngFileUpload']);
-})(angular);
