1ca9f57f9f5fe4bb0dc61c5d273f8770003dcbc0
kevindy <kevindy@abasin.ninja>
2015-05-30 19:59:31 -0600

    Added js hint and nodemon for backend development.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 (function() {
   var gulp = require('gulp');
   var nodemon = require('gulp-nodemon');
+  var jshint = require('gulp-jshint');
 
-  gulp.task('default', function() {
-    console.log('Hello world!');
+  gulp.task('default', ['develop'], function() {
+  });
+
+  gulp.task('develop', function() {
+    nodemon({
+      script: 'backend/app.js',
+      ext: 'js',
+      tasks: ['lint']
+    }).on('restart', function() {
+      console.log('Restarted!');
+    });
+  });
+
+  gulp.task('lint', function() {
+    gulp.src('./**/*.js')
+    .pipe(jshint());
   });
 
 }());
