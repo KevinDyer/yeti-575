891e78d3cc693f7818027a392cfc75087f49dc74
Kevin Dyer <kevin.m.dyer@gmail.com>
2015-06-03 21:10:07 -0600

    Added structure for the fronend.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@
   var app = express();
   var dataRouter = require('./routes/data');
 
+  // Add the data route
   app.use('/data', dataRouter);
 
+  // Add route for bower components
+  app.use('/bower_components', express.static('bower_components'));
+  // Add route for the app
+  app.use(express.static('frontend'));
+
+  // Start the server listening
   app.listen(3000);
 }());
