e4f46911093e2c74d818f21496aade8ebe429dbe
Kevin Dyer <kevin.m.dyer@gmail.com>
2015-06-12 19:31:07 -0600

    backend node app now reads files that were previously uploaded.

--- routes/file.js
+++ routes/file.js
@@ -5,57 +5,88 @@
   var path = require('path');
 
   var UPLOAD_DIR = path.resolve('./uploads');
+  var FILE_LIST_FILENAME = path.resolve(UPLOAD_DIR, 'file-list.json');
 
   var Promise = require('promise');
   var express = require('express');
   var multer = require('multer');
   var Router = express.Router;
   var YetiDbReader = require('./../lib/yeti-db-reader');
+  var UtilFs = require('./../lib/util/util-fs');
 
   var router = new Router();
 
   var sId = 0;
-  var files = [];
+  var fileList = [];
 
   // TODO Read files from the upload directory and analyze them
+  UtilFs.readJSON(FILE_LIST_FILENAME)
+  .then(function(fileList) {
+    return Promise.all(fileList.map(function(file) {
+      return getAndAddDataFromFile(file)
+      .then(null, function(err) {
+        console.log(err);
+      });
+    }));
+  })
+  .then(function() {
+    console.log('Finished loading previously uploaded files.');
+  });
+
+  var getAndAddDataFromFile = function(file) {
+    var yetiDbReader = new YetiDbReader(file.path);
+    return yetiDbReader.getData()
+    .then(function(data) {
+      fileList.push({
+        id: sId++,
+        name: file.name,
+        originalname: file.originalname,
+        path: file.path,
+        data: data,
+        timestamp: file.timestamp || Date.now()
+      });
+      return Promise.resolve(fileList[fileList.length-1]);
+    });
+  };
 
   router.route('/')
   .get(function(req, res) {
-    var filteredFiles = [];
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-      filteredFiles.push({
+    var filteredFiles = fileList.map(function(file) {
+      return {
         id: file.id,
-        displayName: file.displayName,
+        displayName: file.originalname,
         count: Object.keys(file.data).length
-      });
-    }
+      };
+    });
     res.status(200).json(filteredFiles);
   })
   .post(multer({dest: UPLOAD_DIR}), function(req, res) {
     var filenames = Object.keys(req.files);
     Promise.all(filenames.map(function(filename) {
-      var filePath = req.files[filename].path;
-
-      var yetiDbReader = new YetiDbReader(filePath);
-      return yetiDbReader.getData()
-      .then(function(data) {
-        files.push({
-          id: sId++,
-          displayName: req.files[filename].originalname,
-          data: data,
-          timestamp: Date.now()
-        });
-        return Promise.resolve(files[files.length-1]);
-      }, function(err) {
-        console.log(err);
-        return Promise.resolve();
-      });
+      var file = req.files[filename];
+      return getAndAddDataFromFile(file);
     }))
-    .then(function(file) {
-      res.status(204).json(file);
+    .then(function() {
+      return UtilFs.writeJSON(FILE_LIST_FILENAME, fileList.map(function(file) {
+        return {
+          name: file.name,
+          originalname: file.originalname,
+          path: file.path,
+          timestamp: file.timestamp
+        };
+      }));
+    })
+    .then(function() {
+      var filteredFiles = fileList.map(function(file) {
+        return {
+          id: file.id,
+          displayName: file.originalname,
+          count: Object.keys(file.data).length
+        };
+      });
+      res.status(204).json(filteredFiles);
     }, function(err) {
-      res.sendStatus(522);
+      res.status(422).json(err);
     });
   });
 
